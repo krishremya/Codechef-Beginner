215c15b16e7ea899a39bfbd745b95ab56748666d
krishremya <krishremya99@gmail.com>
2018-10-24 18:25:37 +0530

    Sum or Difference

---
+++ sum_or_difference.cpp
@@ -0,0 +1,12 @@
+//Simple program to add or subtract the values according to the conditions.
+#include <iostream>
+using namespace std;
+int main() {
+    int a, b;
+    cin>> a >> b;
+    if(a > b)
+    cout<<a-b;
+    else
+    cout<<a+b;
+    return 0;
+}
