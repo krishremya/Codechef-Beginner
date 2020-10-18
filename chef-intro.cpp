de505243746286e9dcce20c866938ee01eb638cb
Kavya Krishnan <72289432+krishkavya@users.noreply.github.com>
2020-10-18 17:26:19 +0530

    Uploading chef-intro in cpp

---
+++ chef-intro.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n,r,R;
+	cin>>n;
+	cin>>r;
+	for(int i=0;i<n;i++) { {
+	    cin>>R;
+	} if(R<r) {
+	    cout<<"Bad boi"<<endl;
+	} else {
+	    cout<<"Good boi"<<endl;
+	} }
+	return 0;
+}
+
